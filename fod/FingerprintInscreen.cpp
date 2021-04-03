e359a0c57071cf0c4e658f8d5288ffba22c4da6a
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-04-03 07:35:12 +0530

    sm8250-common: improve fod dim calculation
    This is a new algorithm to achieve a better MIUI-Like behaviour
    with grabing some data from stock miui, a profile has been constructed
    and with a proper non-linear regression, a polinomial function has been generated and used to calculate alpha for dim
    
    A new function were generated and fixed to work on certain brightness
    restrictions in the meantime a solid algorithm gets developed.
    
    More info soon...
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -97,18 +97,14 @@ Return<void> FingerprintInscreen::setLongPressEnabled(bool) {
 Return<int32_t> FingerprintInscreen::getDimAmount(int32_t) {
     float alpha;
     int realBrightness = get(BRIGHTNESS_PATH, 0);
-
-    if (realBrightness >= 500) {
-        alpha = 1.0 - pow(realBrightness / 2047.0 * 430.0 / 600.0, 0.485);
-    } else if (realBrightness >= 250) {
-        alpha = 1.0 - pow(realBrightness / 2047.0 * 430.0 / 600.0, 0.530);
-    } else if (realBrightness > 60) {
-         alpha = 1.0 - pow(realBrightness / 1680.0, 0.525);
+    if (realBrightness > 9) {
+    alpha = (255 + ((-8.08071) * pow(realBrightness, 0.45)));
     } else {
-        alpha = 1.0 - pow(realBrightness / 1680.0, 0.475);
+    alpha = (255 + ((-10.08071) * pow(realBrightness, 0.45)));
     }
-
-    return 255 * alpha;
+    if(alpha < 0.82)
+    alpha+=0.1;
+    return alpha;
 }
 
 Return<bool> FingerprintInscreen::shouldBoostBrightness() {
