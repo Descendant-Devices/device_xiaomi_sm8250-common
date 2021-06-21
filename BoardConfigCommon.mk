2543ae1a1ddf4c389f2afecd9c7147572e3ec4db
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-06-21 22:31:56 +0530

    sm8250-common: change defconfig flag to use new common kernel
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -97,7 +97,7 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := vendor/lmi-perf_defconfig
+TARGET_KERNEL_CONFIG := vendor/lmi_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8250
 
 # Kernel modules - WLAN
